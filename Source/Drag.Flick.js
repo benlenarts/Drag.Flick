567a97cbcedb9328c3468407bc8a105c91ba2d82
Ben Lenarts <ben@lenarts.nl>
2009-12-13 16:46:29 -0800

    First commit, adding code and README

---
+++ Source/Drag.Flick.js
@@ -0,0 +1,62 @@
+Drag.Flick = new Class({
+  Extends: Drag,
+
+  constants: { sampleFrequency: 50 },
+
+  initialize: function() {
+    this.options.friction = 0.1;
+    this.options.sensitivity = 0.3;
+    this.options.bounce = 0;
+    
+    this.parent.apply(this, arguments);
+
+    this.addEvent('snap', function(el) {
+      this.sampleHandle = this.sample.periodical(1000 / this.constants.sampleFrequency, this);
+    });
+  },
+
+  start: function(event) {
+    if (this.pusher) this.pusher.cancel();
+
+    this.parent(event);
+
+    this.samples = {};
+    this.speed = {'x': 0, 'y': 0};
+  },
+
+  stop: function(event) {
+    function makeObject(prototype) {
+      function F() {};
+      F.prototype = prototype;
+      return new F();
+    }
+    this.parent(event);
+    $clear(this.sampleHandle);
+    if (this.options.invert) {
+      this.speed.x *= -1;
+      this.speed.y *= -1;
+    }
+    if (this.speed.x || this.speed.y) {
+      var options = makeObject(this.options);
+      options.onComplete = this.fireEvent.bind(this, 'moveEnd');
+      this.pusher = new Fx.Push(this.element, options).start(this.speed.x, this.speed.y);
+    } else {
+      this.fireEvent('moveEnd');
+    }
+  },
+
+  sample: function() {
+    this.samples.last = this.samples.current;
+    this.samples.current = $merge({time: $time()}, this.mouse.now);
+    if (this.samples.last) {
+      var dt = this.samples.current.time - this.samples.last.time;
+      var dx = this.samples.current.x - this.samples.last.x;
+      var dy = this.samples.current.y - this.samples.last.y;
+      var s = this.options.sensitivity;
+      var rs = 1 - s;
+      this.speed.x = this.speed.x * rs + (dx / dt) * s;
+      this.speed.y = this.speed.y * rs + (dy / dt) * s;
+    }
+  }
+
+});
---
+++ Source/Fx.Push.js
@@ -0,0 +1,70 @@
+Fx.Push = new Class({
+
+  Extends: Fx,
+
+	options: {
+    style: true,
+    modifiers: { x: 'left', y: 'top' },
+    friction: 0,
+    limit: false,
+    bounce: 0
+	},
+
+  initialize: function(element, options) {
+    this.element = this.subject = $(element);
+    this.parent(options);
+  },
+
+  start: function(sx, sy) {
+    var m = this.options.modifiers;
+    this.position = {x: (this.options.style ? this.element.getStyle(m.x) : this.element[m.x]).toInt(),
+                     y: (this.options.style ? this.element.getStyle(m.y) : this.element[m.y]).toInt()};
+    this.speed = {x: sx, y: sy};
+    this.time = 0;
+    this.startTimer();
+    this.onStart();
+    this.lastStepTime = this.time;
+    return this;
+  },
+
+  step: function() {
+    var time = $time();
+
+    if (this.options.friction) {
+      if ((Math.abs(this.speed.x) + Math.abs(this.speed.y)) < 0.001) { 
+        this.complete();
+        return;
+      }
+    } else if (time > this.time + this.options.duration) {
+      this.complete();
+      return;
+    } 
+
+    if (this.options.friction) {
+      var multiplier = 1 - this.options.friction;
+      this.speed.x *= multiplier;
+      this.speed.y *= multiplier;
+    }
+
+    var interval = time - this.lastStepTime;
+    this.position.x += this.speed.x * interval;
+    this.position.y += this.speed.y * interval;
+    if (this.options.limit) {
+      var l = this.options.limit;
+      if      (this.position.x < l.x[0]) { this.position.x = l.x[0]; this.speed.x = this.options.bounce * -this.speed.x; }
+      else if (this.position.x > l.x[1]) { this.position.x = l.x[1]; this.speed.x = this.options.bounce * -this.speed.x; }
+      if      (this.position.y < l.y[0]) { this.position.y = l.y[0]; this.speed.y = this.options.bounce * -this.speed.y; }
+      else if (this.position.y > l.y[1]) { this.position.y = l.y[1]; this.speed.y = this.options.bounce * -this.speed.y; }
+    }
+    for (var z in this.options.modifiers){
+      var modifier = this.options.modifiers[z];
+      if (!modifier) continue;
+      this.element[this.options.style ? 'setStyle' : 'set'](modifier, this.position[z].toInt());
+      var value = this.position[z].toInt();
+      if (this.options.style) this.element.setStyle(modifier, value);
+      else this.element[modifier] = value;
+    }
+    this.lastStepTime = time;
+  }
+});
+ 
